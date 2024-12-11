a0bb2e72c4b29d47523524d6677b79c9dd358175
enisabazi <enis.abazi@starlabspro.com>
2024-12-11 15:06:42 +0100

    wip/

--- public/index.php
+++ public/index.php
@@ -10,6 +10,7 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 }
 
 // Register the Composer autoloader...
+// require __DIR__.'/../vendor/autoload.php';
 require __DIR__.'/../vendor/autoload.php';
 // require __DIR__.'/vendor/autoload.php';
 
